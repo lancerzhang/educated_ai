95f90703ed29e71446b65cd8b3a64222480ef313
Sophie Sun <liuyun@sundeMacBook-Air.local>
2018-04-04 21:48:36 +0800

    test filter

--- try/filter1.py
+++ try/filter1.py
@@ -1,10 +1,21 @@
 import numpy as np
-import cv2
+import cv2, time
 
-img = cv2.imread('image2.jpg',0)
-g=np.array([[-1,0,1],[-2,0,2],[-1,0,1]],dtype='float32')
-cov=cv2.filter2D(img,-1,g)
+img = cv2.imread('image2.jpg', 0)
 
-cv2.imshow('image',cov)
+times = 100
+try:
+    while times > 0:
+        times = times - 1
+        filter = np.random.choice([-1, 0, 1], (3, 3))
+        print filter
+        fimg = np.array(filter, dtype='float32')
+        cov = cv2.filter2D(img, -1, fimg)
+        cv2.imshow('image', cov)
+        time.sleep(5)
+
+except KeyboardInterrupt:
+    cv2.destroyAllWindows()
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
