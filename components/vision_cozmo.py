175934339a1d03e90c09c1eb2b10f9dba95633c5
lancerzhang <lancerz@hotmail.com>
2019-12-27 13:57:18 +0800

    cozmo test

---
+++ components/vision_cozmo.py
@@ -0,0 +1,52 @@
+from .vision import Vision
+from . import util
+import cozmo
+import cv2
+import numpy as np
+
+
+class CozmoVision(Vision):
+    ready = False
+    running = True
+    robot = None
+
+    @util.timeit
+    def __init__(self, brain, favor):
+        self.SCREEN_WIDTH = 320
+        self.SCREEN_HEIGHT = 240
+        self.ROI_ARR = [10, 20, 40, 80, 160]
+        super(CozmoVision, self).__init__(brain, favor)
+
+    def run_cozmo_thread(self):
+        print('starting cozmo')
+        cozmo.run_program(self.run_cozmo)
+        while self.running:
+            pass
+
+    def run_cozmo(self, robot: cozmo.robot.Robot):
+        print('run_cozmo')
+        robot.camera.image_stream_enabled = True
+        robot.camera.color_image_enabled = True
+        self.robot = robot
+        self.ready = True
+        print(self.robot)
+
+    @util.timeit
+    def process(self, status, key):
+        print('process')
+        print(self.robot)
+        latest_image = self.robot.world.latest_image.raw_image
+        cv2.namedWindow("frame", cv2.WND_PROP_AUTOSIZE)
+        cv2.setWindowProperty("frame", cv2.WND_PROP_AUTOSIZE, cv2.WND_PROP_AUTOSIZE)
+        cv2.imshow('frame', latest_image)
+        cv2.waitKey(1)
+
+    @util.timeit
+    def grab(self, top, left, width, height):
+        latest_image = self.robot.world.latest_image.raw_image
+        top = int(top)
+        left = int(left)
+        width = int(width)
+        height = int(height)
+        area = (left, top, left + width, top + height)
+        return np.array(latest_image.crop(area))
---
+++ start_cozmo.py
@@ -0,0 +1,113 @@
+from components.action import Action
+from components.brain import Brain
+from components.favor import Favor
+from components.keyboard_listener import KeyboardListener
+from components.mouse_listener import MouseListener
+from components.reward import Reward
+from components.sound_microphone import MicrophoneSound
+from components.vision_cozmo import CozmoVision
+from components.status import Status
+from components import constants
+from components import util
+import logging
+import schedule
+import threading
+import time
+import traceback
+
+logging.basicConfig(filename='app.log', level=logging.INFO,
+                    format='%(asctime)s %(threadName)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+
+
+def save_for_exit(vision, sound):
+    logging.info('saved and exiting...')
+    vision.running = False
+    sound.running = False
+
+
+def run_pending():
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def main():
+    try:
+        logging.info('initializing, please wait.')
+        dps = 1.0 / constants.process_per_second
+        brain = Brain()
+        brain.load()
+        favor = Favor()
+        favor.load()
+        schedule.every(5).seconds.do(brain.save)
+        schedule.every(59).seconds.do(favor.save)
+        schedule_thread = threading.Thread(target=run_pending)
+        schedule_thread.daemon = True
+        schedule_thread.start()
+        reward = Reward(brain)
+        status = Status(brain)
+        action = Action(brain)
+        vision = CozmoVision(brain, favor)
+        vision_thread = threading.Thread(target=vision.run_cozmo_thread)
+        vision_thread.daemon = True
+        vision_thread.start()
+        sound = MicrophoneSound(brain, favor)
+        sound_thread = threading.Thread(target=sound.receive)
+        sound_thread.daemon = True
+        sound_thread.start()
+        mouse_listener = MouseListener()
+        mouse_thread = threading.Thread(target=mouse_listener.run)
+        mouse_thread.daemon = True
+        mouse_thread.start()
+        keyboard_listener = KeyboardListener()
+        keyboard_thread = threading.Thread(target=keyboard_listener.run)
+        keyboard_thread.daemon = True
+        keyboard_thread.start()
+        frames = 0
+        last_process_time = 0
+        logging.info('initialized.')
+        print('initialized.')
+        while 1:
+            while vision.ready:
+                print(f'cozmo is ready')
+                logging.info('frame started.')
+                start = time.time()
+                button = mouse_listener.get_button()
+                key = keyboard_listener.get_key()
+                if key is constants.KEY_SHIFT:
+                    brain.save()
+                    favor.save()
+                    save_for_exit(vision, sound)
+                    break
+
+                logging.debug('frame is {0} '.format(frames))
+                frames = frames + 1
+
+                # brain.associate()
+                # brain.activate_children()
+                focus = vision.process(status, key)
+                # sound.process(status)
+                # action.process(status, button, focus)
+                # reward.process(key)
+                # brain.match_memories()
+                # brain.compose_memories()
+                # brain.cleanup()
+
+                all_duration = util.time_diff(start)
+                logging.info('frame took %d ms' % (all_duration * 1000))
+
+                # all end, sleep to avoid running too fast
+                if all_duration < dps:
+                    time.sleep(dps - all_duration)
+                elif (last_process_time + all_duration) < dps * 2:
+                    time.sleep(dps * 2 - last_process_time - all_duration)
+                last_process_time = all_duration
+
+    except KeyboardInterrupt:
+        save_for_exit(None)
+    except:
+        logging.error(traceback.format_exc())
+
+
+if __name__ == "__main__":
+    main()
