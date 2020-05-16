219ad6e8e7d3823dd80a4da338ac9ba99b7fe389
lancerzhang <lancerz@hotmail.com>
2020-05-16 15:58:36 +0800

    cleanup code

--- try/db_tinydb_smartcache1.py
+++ try/db_tinydb_smartcache1.py
@@ -6,10 +6,15 @@ db = TinyDB(storage=MemoryStorage)
 db.table_class = SmartCacheTable
 table = db.table('Memory')
 # below is a memory, which can be a Q(s,a)
-docId1 = table.insert({'strength': 100, 'type': 'memory', 'recall': 1, 'reward': 1, 'lastRecall': 20180106, 'children': [123, 123]})
-docId2 = table.insert({'strength': 100, 'type': 'vision', 'recall': 1, 'reward': 1, 'lastRecall': 20180106, 'filter': 'vf1', 'data': 'abc'})
+docId1 = table.insert(
+    {'strength': 100, 'type': 'memory', 'recall': 1, 'reward': 1, 'lastRecall': 20180106, 'children': [123, 123]})
+docId2 = table.insert(
+    {'strength': 100, 'type': 'vision', 'recall': 1, 'reward': 1, 'lastRecall': 20180106, 'filter': 'vf1',
+     'data': 'abc'})
 table.insert({'strength': 99, 'type': 'sound', 'recall': 1, 'reward': 1, 'lastRecall': 20180106, 'data': '123'})
-table.insert({'strength': 99, 'type': 'move_vision', 'recall': 1, 'reward': 1, 'lastRecall': 20180106, 'angle': 10, 'speed': 10, 'duration': 10})
+table.insert(
+    {'strength': 99, 'type': 'move_vision', 'recall': 1, 'reward': 1, 'lastRecall': 20180106, 'angle': 10, 'speed': 10,
+     'duration': 10})
 table.insert({'strength': 99, 'type': 'speak', 'recall': 1, 'reward': 1, 'lastRecall': 20180106, 'word': 'yes'})
 
 Memory = Query()
--- try/microphone_recognition1.py
+++ try/microphone_recognition1.py
@@ -5,11 +5,13 @@ m = sr.Microphone()
 
 try:
     print("A moment of silence, please...")
-    with m as source: r.adjust_for_ambient_noise(source)
+    with m as source:
+        r.adjust_for_ambient_noise(source)
     print(("Set minimum energy threshold to {}".format(r.energy_threshold)))
     while True:
         print("Say something!")
-        with m as source: audio = r.listen(source)
+        with m as source:
+            audio = r.listen(source)
         print("Got it! Now to recognize it...")
         try:
             # recognize speech using Google Speech Recognition
