08643fdae2c6fce6a95321b2e34e604bb581bfd1
lancerzhang <35510642+lancerzhang@users.noreply.github.com>
2020-11-26 09:14:43 +0800

    add var type

--- components/brain.py
+++ components/brain.py
@@ -9,6 +9,7 @@ import vptree
 
 from components import constants
 from components import util
+from components.features import Feature
 from components.memory import Memory
 from components.recognizers import ImageRecognizer
 from components.recognizers import VoiceRecognizer
@@ -50,7 +51,7 @@ class Brain:
     def stop(self):
         self.running = False
 
-    def process_voice(self, voice_features_serial):
+    def process_voice(self, voice_features_serial: list):
         packs = []
         for features in voice_features_serial:
             data = self.input_real(features)
@@ -63,7 +64,7 @@ class Brain:
         short = self.input_memory(constants.short, instants)
 
     @util.timeit
-    def input_real(self, features):
+    def input_real(self, features: list):
         data = set()
         for feature in features:
             m = self.find_real(feature)
@@ -75,7 +76,7 @@ class Brain:
         return data
 
     @util.timeit
-    def input_memory(self, memory_type, data):
+    def input_memory(self, memory_type: str, data: list):
         if constants.memory_types.index(memory_type) <= constants.memory_types.index(constants.instant):
             # for instant and below memory, require more at least one child
             if len(data) == 0:
@@ -104,7 +105,7 @@ class Brain:
                 m.data_indexes.add(memory.MID)
         return memory
 
-    def add_seq(self, mm, old, n_limit=-1, time_limit=-1):
+    def add_seq(self, mm: Memory, old: list, n_limit=-1, time_limit=-1):
         ls = []
         if len(old) > 0:
             for x in old:
@@ -131,7 +132,7 @@ class Brain:
         else:
             return ls
 
-    def add_memory(self, memory_type, memory_data, real_type=None):
+    def add_memory(self, memory_type: str, memory_data: list, real_type: str = None):
         m = Memory(memory_type, memory_data, real_type)
         if real_type:
             memory_type = real_type
@@ -140,7 +141,7 @@ class Brain:
         self.memory_cache[memory_type].add(m)
         return m
 
-    def find_real_cache(self, feature):
+    def find_real_cache(self, feature: Feature):
         recognizer = self.RECOGNIZERS[feature.type]
         cache = self.memory_cache[feature.type].copy()
         # print(f'len cache {len(cache)}')
@@ -148,7 +149,7 @@ class Brain:
             if recognizer.is_feature_similar(feature, m.data):
                 return m
 
-    def find_real_tree(self, feature):
+    def find_real_tree(self, feature: Feature):
         recognizer = self.RECOGNIZERS[feature.type]
         tree = self.memory_vp_tree[feature.type]
         if tree is not None:
@@ -158,14 +159,14 @@ class Brain:
                 return nearest_memory
 
     @util.timeit
-    def find_real(self, feature):
+    def find_real(self, feature: Feature):
         nearest = self.find_real_tree(feature)
         if nearest is None:
             nearest = self.find_real_cache(feature)
         return nearest
 
     @util.timeit
-    def find_memory(self, memory_type, child_memories):
+    def find_memory(self, memory_type: str, child_memories: list):
         if len(child_memories) == 0:
             return
         if constants.ordered == self.get_order(memory_type):
@@ -216,9 +217,9 @@ class Brain:
         return found_memory
 
     @staticmethod
-    def get_order(memory_type):
+    def get_order(memory_type: str):
         order = constants.ordered  # ordered
-        if constants.memory_types.index(memory_type) <= constants.memory_types.index(constants.pack):
+        if constants.memory_types.index(memory_type) <= constants.memory_types.index(constants.context):
             order = constants.unordered  # unordered
         return order
 
@@ -251,7 +252,7 @@ class Brain:
         return round(r, 2)
 
     @classmethod
-    def is_steady(cls, m):
+    def is_steady(cls, m: Memory):
         now_time = time.time()
         # print(f'now {now_time}, at {m.activated_time}, stb {m.stability}')
         if now_time < m.activated_time + cls.memory_cycles[m.stability]:
@@ -260,7 +261,7 @@ class Brain:
             return False
 
     @classmethod
-    def activate_memory(cls, m):
+    def activate_memory(cls, m: Memory):
         now_time = time.time()
         if m.stability == len(cls.memory_cycles):
             return
@@ -273,7 +274,7 @@ class Brain:
         m.activated_time = now_time
 
     @classmethod
-    def validate_memory(cls, m):
+    def validate_memory(cls, m: Memory):
         now_time = time.time()
         if cls.is_steady(m):
             # print(f'is steady')
@@ -344,7 +345,6 @@ class Brain:
 
     def cleanup_memories(self):
         new_all_memories = {}
-        debuged = False
         for ft in constants.feature_types + constants.memory_types:
             memories = self.categorized_memory[ft].copy()
             new_memories = {}
